b336adb2f9ec51a22b46347b1846cbe8f35dc2cb
Anuj Gupta <anuj.gupta@dropneed.com>
2018-08-21 15:47:36 +0530

    Delete commented line for Db

--- config/database.js
+++ config/database.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-//mongoose.connect('mongodb://localhost:27017/shoppingcart');
 mongoose.connect('mongodb://anujshop:Anuj1234@ds125402.mlab.com:25402/anujshop');
 
 // Get Mongoose to use the global promise library
